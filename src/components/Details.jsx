bba6d567642e9a6b7f7b354c8a66460347cbf26e
Ankit Niranjan <ankitniranjan331@gmail.com>
2025-01-15 14:00:00 +0530

    Home and Details Page Complete

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import axios from '../utils/axios'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Loading from './Loading';
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  const [product, setproduct] = useState(null)
+  const { id } = useParams();
+  console.log(id)
+   const getSingleProduct = async () => {
+      try {
+        const { data } = await axios.get(`/products/${id}`);
+        setproduct(data)
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    };
 
-export default Details
+  useEffect(() => {
+    getSingleProduct();
+  }, []);
+
+  return ( product ?
+    <div className="m-auto w-[70%] flex justify-between items-center h-full p-[10%]">
+      <img
+        className="object-contain h-[80%] w-[40%]"
+        src={`${product.image}`}
+        alt=""
+      />
+      <div className="content w-[50%]">
+        <h1 className="text-4xl">
+          {product.title}
+        </h1>
+        <h3 className="text-zinc-500 my-5">{product.category}</h3>
+        <h2 className="text-red-300 mb-3">INR {product.price*80}</h2>
+        <p className="mb-[5%]">
+          {product.description}
+        </p>
+        <Link className="py-2 px-5 mr-5 border rounded-lg border-blue-200 text-blue-300">
+          Edit
+        </Link>
+        <Link className="py-2 px-5 border rounded-lg border-red-200 text-red-300">
+          Delete
+        </Link>
+      </div>
+    </div>:(
+      <Loading />
+    )
+  );
+};
+
+export default Details;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import Nav from "./Nav";
 import { Link } from "react-router-dom";
+import { ProductContext } from "../utils/Context";
+import Loading from "./Loading";
 
 const Home = () => {
-  return (
+  const [products] = useContext(ProductContext);
+  console.log(products);
+  return products ? (
     <>
       <Nav />
       <div className="main w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
-        <Link to='/details/1' className="card p-3 mr-3 mb-3 border rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 transition ease-in mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1 className="hover:text-blue-300">Lorem ipsum dolor sit amet.</h1>
-        </Link>
+        {products.map((p, i) => {
+          return (
+            <Link key={p.id}
+              to={`/details/${p.id}`}
+              className="card p-3 mr-3 mb-3 border rounded shadow w-[18%] h-[30vh] flex flex-col justify-center items-center"
+            >
+              <div
+                className="hover:scale-110 transition ease-in mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
+                style={{
+                  backgroundImage: `url(${p.image})`,
+                }}
+              ></div>
+              <h1 className="hover:text-blue-300">{p.title}</h1>
+            </Link>
+          );
+        })}
       </div>
     </>
+  ) : (
+    <Loading />
   );
 };
 
