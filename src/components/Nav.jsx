b328db1777842dd634d30830c3f880b70f8d802a
Ankit Niranjan <ankitniranjan331@gmail.com>
2025-01-16 15:00:00 +0530

    Dynamic Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,36 +1,49 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ProductContext } from "../utils/Context";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const [products] = useContext(ProductContext);
+  let distinct_category =
+    products && products.reduce((acc, cv) => [...acc, cv.category], []);
+  distinct_category = [...new Set(distinct_category)];
+  console.log(distinct_category);
+
+  const color = () => {
+    return `rgba(${(Math.random() * 255).toFixed()},${(
+      Math.random() * 255
+    ).toFixed()},${(Math.random() * 255).toFixed()},0.6)`;
+  };
+
   return (
     <nav className="w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5">
-        <a
-          className="py-2 px-5 border rounded-lg border-blue-200 text-blue-300"
-          href="/create"
-        >
-          Add New Product
-        </a>
-        <hr className="w-[80%] my-3" />
-        <h1 className="text-2xl mb-3 w-[80%] font-medium">Category Filter</h1>
-        <ul className=" w-[80%] font-medium">
-          <li className="flex items-center mb-3">
-            <span className="mr-2 rounded-full w-[15px] h-[15px] bg-blue-400"></span>{" "}
-            Cat 1
-          </li>
-          <li className="flex items-center mb-3">
-            <span className="mr-2 rounded-full w-[15px] h-[15px] bg-yellow-400"></span>{" "}
-            Cat 2
-          </li>
-          <li className="flex items-center mb-3">
-            <span className="mr-2 rounded-full w-[15px] h-[15px] bg-red-400"></span>{" "}
-            Cat 3
-          </li>
-          <li className="flex items-center mb-3">
-            <span className="mr-2 rounded-full w-[15px] h-[15px] bg-green-400"></span>{" "}
-            Cat 4
-          </li>
-        </ul>
-      </nav>
-  )
-}
+      <a
+        className="py-2 px-5 border rounded-lg border-blue-200 text-blue-300"
+        href="/create"
+      >
+        Add New Product
+      </a>
+      <hr className="w-[80%] my-3" />
+      <h1 className="text-2xl mb-3 w-[80%] font-medium">Category Filter</h1>
+      <div className=" w-[80%]">
+        {distinct_category.map((c, i) => {
+          return (
+            <Link
+              key={i}
+              to={`/?category=${c}`}
+              className="flex items-center mb-3"
+            >
+              <span
+                style={{ backgroundColor: color() }}
+                className="mr-2 rounded-full w-[15px] h-[15px]"
+              ></span>{" "}
+              {c}
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
