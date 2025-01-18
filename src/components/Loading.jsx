1fd794fadae25634e3c9d4a03be4c2c1748d3bc4
Ankit Niranjan <ankitniranjan331@gmail.com>
2025-01-18 10:00:00 +0530

    Loading Animation Fixed

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { Orbit } from '@uiball/loaders'
 
 const Loading = () => {
+  const color = () => {
+    return `rgba(${(Math.random() * 255).toFixed()},${(
+      Math.random() * 255
+    ).toFixed()},${(Math.random() * 255).toFixed()})`;
+  };
   return (
     <div className='h-full w-full flex items-center justify-center'>
-      <Orbit size={40} speed={1.5} color="#32cd32" />
+      <Orbit size={40} speed={1.5} color={color()} />
     </div>
   )
 }
